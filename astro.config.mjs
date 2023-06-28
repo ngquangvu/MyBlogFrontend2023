6de52da9eda1e51d556b9f193e27f6d57750639a
ngquangvu <nguyenquangvu.work@gmail.com>
2023-06-28 21:48:58 +0700

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,10 +12,15 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind({
-      config: { applyBaseStyles: false, path: "./custom-config.cjs" },
+      config: {
+        applyBaseStyles: false,
+        path: "./custom-config.cjs",
+      },
     }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
+  output: "server",
+  adapter: netlify(),
 });
